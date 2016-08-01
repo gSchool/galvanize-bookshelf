c3ac29287540d0a71a27afbd8c884441855665ed
Ryan Sobol <contact@ryansobol.com>
2016-08-01 07:03:08 -0700

    Update server.js file

--- server.js
+++ server.js
@@ -1,46 +1,58 @@
 'use strict';
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ silent: true });
+  require('dotenv').config();
 }
 
 const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8000;
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
-
-const authors = require('./routes/authors');
-const books = require('./routes/books');
-const session = require('./routes/session');
-const users = require('./routes/users');
-const users_books = require('./routes/users_books');
-
 const app = express();
 
 app.disable('x-powered-by');
 
-if (process.env.NODE_ENV !== 'test') {
-  const morgan = require('morgan');
-  app.use(morgan('short'));
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const morgan = require('morgan');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
 }
 
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(cookieSession({
   name: 'bookshelf',
-  secret: process.env.SESSION_SECRET
+  secret: process.env.SESSION_SECRET,
+  secureProxy: app.get('env') === 'production'
 }));
+
+const path = require('path');
 
 app.use(express.static(path.join('public')));
 
-app.use(authors);
+app.use((req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+const books = require('./routes/books');
+const favorites = require('./routes/favorites');
+const session = require('./routes/session');
+const users = require('./routes/users');
+
 app.use(books);
+app.use(favorites);
 app.use(session);
 app.use(users);
-app.use(users_books);
 
 app.use((_req, res) => {
   res.sendStatus(404);
@@ -48,13 +60,22 @@ app.use((_req, res) => {
 
 // eslint-disable-next-line max-params
 app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
   // eslint-disable-next-line no-console
   console.error(err.stack);
   res.sendStatus(500);
 });
 
+const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (app.get('env') !== 'test') {
     // eslint-disable-next-line no-console
     console.log('Listening on port', port);
   }
