63faf88beb2e363d95fea238c48454fa1c592abb
Ryan Sobol <contact@ryansobol.com>
2016-08-01 07:06:24 -0700

    Update the routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+
 const router = express.Router();
 
 // YOUR CODE HERE
--- routes/users_books.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-
-// YOUR CODE HERE
-
-module.exports = router;
