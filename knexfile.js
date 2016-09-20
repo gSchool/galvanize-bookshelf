2aa01ebf9f80644924bf782e09b86cf442c9109b
BAMason <4emason@gmail.com>
2016-09-20 15:10:24 -0600

    part1 passing

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,13 @@
+// Update with your config settings.
 'use strict';
 
 module.exports = {
-  development: {},
-
-  test: {},
-
-  production: {}
+  development: {
+    client: `pg`,
+    connection: `postgres://localhost/bookshelf_dev`
+  },
+  test: {
+    client: `pg`,
+    connection: `postgres://localhost/bookshelf_test`
+  }
 };
