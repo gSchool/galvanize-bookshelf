169b46c404bf36b4a3ac64c669ae8c4435581515
BAMason <4emason@gmail.com>
2016-09-22 22:35:53 -0600

    part4 tests passing

--- routes/session.js
+++ routes/session.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-sync */
 'use strict';
 
 const express = require(`express`);
@@ -9,7 +10,36 @@ const router = express.Router();
 const knex = require(`../knex`);
 const humps = require(`humps`);
 const bcrypt = require(`bcrypt`);
+const boom = require(`boom`);
 
+router.get(`/session`, (req, res) => {
+  if (req.session.userId) { res.json(true); }
+  else { res.json(false); }
+});
 
+router.post(`/session`, (req, res, next) => {
+  const user = req.body;
+
+  knex(`users`)
+  .where(`email`, user.email)
+  .select(`id`, `first_name`, `last_name`, `email`, `hashed_password`)
+  .first()
+  .then((result) => {
+    if (result && bcrypt.compareSync(user.password, result.hashed_password)) {
+      delete result.hashed_password;
+      req.session.userId = result;
+      res.json(humps.camelizeKeys(result));
+    }
+    else {
+      throw boom.create(400, 'Bad email or password');
+    }
+  })
+  .catch((err) => { next(err); });
+});
+
+router.delete(`/session`, (req, res) => {
+  req.session = null;
+  res.json(true);
+});
 
 module.exports = router;
