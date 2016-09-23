64598c9a91a08c12b2bf849ec864616b411b9a8e
BAMason <4emason@gmail.com>
2016-09-23 17:37:29 -0600

    updated favorites route

--- routes/favorites.js
+++ routes/favorites.js
@@ -6,5 +6,28 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+const knex = require(`../knex`);
+const humps = require(`humps`);
+
+router.get(`/favorites`, (req, res, next) => {
+  if (req.session.userId) {
+    knex(`favorites`)
+    .where(`user_id`, req.session.userId.id)
+    .first()
+    .then((results) => {
+      knex(`books`)
+      .where(`id`, results.book_id)
+      .then((list) => {
+        list.forEach((book) => {
+          book.bookId = book.id;
+          delete book.id;
+        });
+        res.json(humps.camelizeKeys(list));
+      })
+      .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+  }
+});
 
 module.exports = router;
