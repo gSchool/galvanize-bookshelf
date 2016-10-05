80d5bca6628f78ce55fb874f8a19ef30d6674fff
BAMason <4emason@gmail.com>
2016-10-05 14:15:00 -0600

    linting

--- routes/books.js
+++ routes/books.js
@@ -5,54 +5,73 @@ const express = require(`express`);
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
 // const boom = require('boom');
 const knex = require(`../knex`);
 const humps = require(`humps`);
 
-router.get(`/`, (req, res) => {
-  knex(`books`).orderBy(`title`).then((data) => {
-    res.json(humps.camelizeKeys(data));
-  });
+router.get(`/`, (req, res, next) => {
+  knex(`books`)
+  .orderBy(`title`)
+  .then((data) => { res.json(humps.camelizeKeys(data)); })
+  .catch((err) => next(err));
 });
 
-router.get(`/:id`, (req, res) => {
+router.get(`/:id`, (req, res, next) => {
   const id = req.params.id;
-  knex(`books`).where(`id`, id).first().then((data) => {
-    res.json(humps.camelizeKeys(data));
-  });
+  knex(`books`)
+  .where(`id`, id)
+  .first()
+  .then((data) => { res.json(humps.camelizeKeys(data)); })
+  .catch((err) => next(err));
 });
 
-router.post(`/`, (req, res) => {
-  knex(`books`).insert(humps.decamelizeKeys(req.body), `id`).then((num) => {
+router.post(`/`, (req, res, next) => {
+  knex(`books`)
+  .insert(humps.decamelizeKeys(req.body), `id`)
+  .then((num) => {
     const id = num[0];
-    knex(`books`).where(`id`, id).first().then((data) => {
-      res.json(humps.camelizeKeys(data));
-    });
-  });
+    knex(`books`)
+    .where(`id`, id)
+    .first()
+    .then((data) => { res.json(humps.camelizeKeys(data)); })
+    .catch((e) => next(e));
+  })
+  .catch((err) => next(err));
 });
 
 // run the below command in bookshelf_test before every attempt to pass
 // ALTER SEQUENCE books_id_seq RESTART WITH 9;
-router.patch(`/:id`, (req, res) => {
+router.patch(`/:id`, (req, res, next) => {
   const id = req.params.id;
-  knex(`books`).where(`id`, id).update(humps.decamelizeKeys(req.body)).then(() => {
-    knex(`books`).where(`id`, id).first().then((data) => {
-      res.json(humps.camelizeKeys(data));
-    });
-  });
+  knex(`books`)
+  .where(`id`, id)
+  .update(humps.decamelizeKeys(req.body))
+  .then(() => {
+    knex(`books`)
+    .where(`id`, id)
+    .first()
+    .then((data) => { res.json(humps.camelizeKeys(data)); })
+    .catch((e) => next(e));
+  })
+  .catch((err) => next(err));
 });
 
-router.delete(`/:id`, (req, res) => {
+router.delete(`/:id`, (req, res, next) => {
   const id = req.params.id;
   let book;
-  knex(`books`).select(`author`, `cover_url`, `description`, `genre`, `title`)
-  .where(`id`, id).first().then((data) => {
+  knex(`books`)
+  .select(`author`, `cover_url`, `description`, `genre`, `title`)
+  .where(`id`, id)
+  .first()
+  .then((data) => {
     book = humps.camelizeKeys(data);
-    knex(`books`).where(`id`, id).del().then(() => {
-      res.send(book);
-    });
-  });
+    knex(`books`)
+    .where(`id`, id)
+    .del()
+    .then(() => { res.send(book); })
+    .catch((e) => next(e));
+  })
+  .catch((err) => next(err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 'use strict';
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== `production`) {
+  require(`dotenv`).config();
 }
 
-const express = require('express');
+const express = require(`express`);
 const app = express();
 
-app.disable('x-powered-by');
+app.disable(`x-powered-by`);
 
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const morgan = require('morgan');
+const bodyParser = require(`body-parser`);
+const cookieSession = require(`cookie-session`);
+const morgan = require(`morgan`);
 
-switch (app.get('env')) {
-  case 'development':
-    app.use(morgan('dev'));
+switch (app.get(`env`)) {
+  case `development`:
+    app.use(morgan(`dev`));
     break;
 
-  case 'production':
-    app.use(morgan('short'));
+  case `production`:
+    app.use(morgan(`short`));
     break;
 
   default:
@@ -27,28 +27,28 @@ switch (app.get('env')) {
 
 app.use(bodyParser.json());
 app.use(cookieSession({
-  name: 'bookshelf',
+  name: `bookshelf`,
   secret: process.env.SESSION_SECRET,
-  secureProxy: app.get('env') === 'production'
+  secureProxy: app.get(`env`) === `production`
 }));
 
-const path = require('path');
+const path = require(`path`);
 
-app.use(express.static(path.join('public')));
+app.use(express.static(path.join(`public`)));
 
 // CSRF protection
 app.use((req, res, next) => {
-  if (/json/.test(req.get('Accept'))) {
+  if (/json/.test(req.get(`Accept`))) {
     return next();
   }
 
   res.sendStatus(406);
 });
 
-const books = require('./routes/books');
-const favorites = require('./routes/favorites');
-const session = require('./routes/session');
-const users = require('./routes/users');
+const books = require(`./routes/books`);
+const favorites = require(`./routes/favorites`);
+const session = require(`./routes/session`);
+const users = require(`./routes/users`);
 
 app.use(`/books`, books);
 app.use(`/favorites`, favorites);
@@ -64,7 +64,7 @@ app.use((err, _req, res, _next) => {
   if (err.output && err.output.statusCode) {
     return res
       .status(err.output.statusCode)
-      .set('Content-Type', 'text/plain')
+      .set(`Content-Type`, `text/plain`)
       .send(err.message);
   }
 
@@ -76,9 +76,9 @@ app.use((err, _req, res, _next) => {
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  if (app.get('env') !== 'test') {
+  if (app.get(`env`) !== `test`) {
     // eslint-disable-next-line no-console
-    console.log('Listening on port', port);
+    console.log(`Listening on port`, port);
   }
 });
 
