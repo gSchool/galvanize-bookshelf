6a304a643cd63b98d9dbff150a9b575e022d6641
Michael Friedman <contact@michaeldavidfriedman.com>
2017-01-04 15:19:09 -0800

    Migration and seed tests passing.

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = {
-  development: {},
-
-  test: {},
-
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_test'
+  },
   production: {}
 };
