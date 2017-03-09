31c4a89ffffa3d67dd5aba6d2a4bf2129c10eaa9
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-03-08 19:43:19 -0800

    I passed the test, but need to figure out what returning * do

--- migrations/20170307133507_books.js
+++ migrations/20170307133507_books.js
@@ -9,25 +9,8 @@ exports.up = function(knex, Promise) {
     table.text('description').notNullable().defaultTo('');
     table.text('cover_url').notNullable().defaultTo('');
     table.timestamps(true, true)
-
   });
 };
-
-//
-// exports.up = function(knex) {
-//   return knex.schema.createTable('tracks', (table) => {
-//     table.increments();
-//     table.integer('artist_id')
-//       .notNullable()
-//       .references('id')
-//       .inTable('artists')
-//       .onDelete('CASCADE')
-//       .index();
-//     table.string('title').notNullable().defaultTo('');
-//     table.integer('likes').notNullable().defaultTo(0);
-//     table.timestamps(true, true);
-//   });
-// };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable("books");
