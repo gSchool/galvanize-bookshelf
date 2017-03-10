6179a31de2821041d935cb24496c6ebc5a7e1d3b
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-03-10 01:29:46 -0800

    finsihed part 3

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,35 @@
 'use strict';
-
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex.js');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+    bcrypt.hash(req.body.password, 12)
+        .then((hashed_password) => {
+            //console.log('testing!',req.body.firstName);
+            return knex('users')
+                .insert({
+                    first_name: req.body.firstName,
+                    last_name: req.body.lastName,
+                    email: req.body.email,
+
+                    hashed_password: hashed_password
+                }).returning('*');
+        })
+        .then((users) => {
+            //console.log(users);
+            let user = users[0];
+            //  console.log('before I camelize', user);
+            let camelizeResult = humps.camelizeKeys(user);
+            delete camelizeResult.hashedPassword;
+            //  console.log('i am here',camelizeResult);
+            res.status(200).json(camelizeResult);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
 
 module.exports = router;
--- seeds/books.js
+++ seeds/books.js
@@ -79,9 +79,8 @@ exports.seed = function(knex, Promise) {
                         updated_at: new Date('2016-06-26 14:26:16 UTC')
                     }]
                 )
-
             ]);
-        }).then( ()=>{
-          return knex.raw("SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))");
+        }).then(() => {
+            return knex.raw("SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))");
         });
 };
