8fa2c2b039ffa28fb9a9f67ddc36b42b17bbf976
Brian James <brianpja@gmail.com>
2017-08-01 16:28:37 -0700

    passing all tests part 3

--- seeds/2_users.js
+++ seeds/2_users.js
@@ -14,5 +14,9 @@ exports.seed = function(knex, Promise) {
           updated_at: new Date('2016-06-29 14:26:16 UTC')
         }]),
       ]);
+    })
+    .then(function() {
+      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));")
     });
+
 };
