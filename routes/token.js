5b1f1eeb18ea02364b8b76e6cc677f75daadf722
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-02 15:21:04 -0700

    so very lost and confused

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex.js');
+
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/token', (req, res, next) => {
+  knex('users')
+})
 
 module.exports = router;
