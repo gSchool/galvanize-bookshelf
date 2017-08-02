37cadf071a22f3f5760b99b203ceb1931460670b
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-02 16:23:48 -0700

    wut

--- routes/token.js
+++ routes/token.js
@@ -8,7 +8,6 @@ const knex = require('../knex.js');
 const errHandle = require('../errHandle.js')
 
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.get('/token', (req, res) => {
@@ -16,10 +15,42 @@ router.get('/token', (req, res) => {
     if (err) {
       return res.send(false);
     }
-
     res.send(true);
   });
 });
+
+router.post('/token', (req, res, next) => {
+  let user;
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((user) => {
+      if (!user){
+        throw errHandle(400, 'Bad email or password')
+      }
+      user = user
+      return bcrypt.compare(req.body.password, user.hashed_password)
+    })
+    .then(() => {
+      const claim = {userId: user.id}
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '7 days'
+      });
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 640800000),
+        secure: router.get('env') === 'production'
+    })
+    delete user.hashed_password;
+    res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw errHandle(400, 'Bad email or password')
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
 router.delete('/token', (req, res) => {
   res.clearCookie('token');
