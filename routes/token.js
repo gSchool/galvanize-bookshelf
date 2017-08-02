228b774e2bca9fc541c1dc228754455812d300b9
Brian James <brianpja@gmail.com>
2017-08-02 16:25:47 -0700

    part 4 passing tests

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,72 @@
 'use strict';
 
+const boom = require('boom');
+const bcrypt = require('bcrypt-as-promised');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 // YOUR CODE HERE
+
+router.post('/token', (req, res, next) => {
+  let user;
+
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password');
+      }
+
+      user = camelizeKeys(row);
+
+      return bcrypt.compare(req.body.password, user.hashedPassword);
+    })
+    .then(() => {
+      const claim = { userId: user.id };
+
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '7 days'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        secure: router.get('env') === 'production'
+      });
+
+      delete user.hashedPassword;
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () =>{
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+
+router.get('/token', (req, res) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, _payload) => {
+    if (err) {
+      return res.send(false);
+    }
+
+    res.send(true);
+  })
+})
+
+router.delete('/token', (req, res) => {
+  res.clearCookie('token');
+  res.end();
+});
+
 
 module.exports = router;
