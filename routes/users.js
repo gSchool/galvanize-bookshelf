31734a4fc9bc5be7c2e3d1308643bd779c1c6fa9
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-02 11:05:57 -0700

    got school on bcrypt because I don't understand it, and I still don't understand it

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-as-promised');
 const express = require('express');
 const knex = require('../knex.js');
 // eslint-disable-next-line new-cap
@@ -9,17 +9,24 @@ const router = express.Router();
 // YOUR CODE HERE
 router.post('/users', (req, res, next) => {
   const{firstName, lastName, email, password} = req.body
-  const hashed_password = bcrypt.hash(password, 12)
-  return knex("user")
-      .insert({
-        'first_name': firstName,
-        'last_name': lastName,
-        'email': email,
-        'hashed_password': hashed_password
-      }, '*')
+  bcrypt.hash(password, 12).then((hashedPassword) =>{
+    return knex("users")
+    .insert({
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      'hashed_password': hashedPassword
+    }, '*')
+  })
+      // .returning('id', 'first_name as firstName', 'last_name as lastName', 'email')
 
       .then((user) => {
-        res.send(user[0])
+        return res.send({
+          id: user[0].id,
+          firstName: user[0].first_name,
+          lastName: user[0].last_name,
+          email: user[0].email
+        })
       })
       .catch ((err) => {
         return next(err)
--- seeds/01_users.js
+++ seeds/01_users.js
@@ -15,5 +15,8 @@ exports.seed = function(knex, Promise) {
           updated_at: new Date('2016-06-29 14:26:16 UTC')
         })
       ]);
+    })
+    .then(() => {
+      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));")
     });
 };
