c22b345d338be6b2c9e6bf43dc11ed7504988420
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-03 09:42:32 -0700

    passing the first part of five

--- migrations/20170803092638_favorites.js.js
+++ migrations/20170803092638_favorites.js.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments();
+    table.integer('user_id').references('users.id').notNullable().onDelete('CASCADE').index();
+    table.integer('book_id').references('books.id').notNullable().onDelete('CASCADE').index();
+    table.timestamps(true, true);
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('favorites');
 };
