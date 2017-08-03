d3c537d25819a76e9b7a028abc891d4119678015
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-03 12:50:19 -0700

    don't nest your thens in other thens

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
+const errHandle = require('../errHandle.js');
+const knex = require('../knex.js');
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+
 
 module.exports = router;
