f48d4b1174347207107b141f37310317a2b22c32
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-03 10:47:17 -0700

    finished part5 seeds

--- seeds/03_favorites.js
+++ seeds/03_favorites.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('favorites').del()
     .then(function () {
       return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('favorites').insert([{
+          id: 1,
+          book_id: 1,
+          user_id: 1,
+          created_at: new Date('2016-06-29 14:26:16 UTC'),
+          updated_at: new Date('2016-06-29 14:26:16 UTC')
+        }])
       ]);
     });
 };
