2c354999e33099961c2d43a950ce7073f23ab34f
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-03 15:46:08 -0700

    I don't know nothing

--- routes/favorites.js
+++ routes/favorites.js
@@ -10,6 +10,20 @@ const {camelizeKeys, decamelizeKeys} = require('humps');
 const router = express.Router();
 
 router.get('/favorites', authorize, (req, res, next) => {
+  // console.log(req.query);
+ knex('favorites')
+  .innerJoin('books', 'books.id', 'favorites.book_id')
+  .where('favorites.user_id', req.claim.userId)
+  .then((faves) => {
+    res.send(camelizeKeys(faves))
+  })
+  .catch((err) => {
+    return next(err)
+  })
+})
+
+router.get('/favorites/check', authorize, (req, res, next) => {
+  console.log(req.query);
  knex('favorites')
   .innerJoin('books', 'books.id', 'favorites.book_id')
   .where('favorites.user_id', req.claim.userId)
