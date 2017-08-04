88b4f2db31ff052689cf66b596efe59facaad5dc
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-04 08:55:23 -0700

    wut

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,8 @@ module.exports = {
     connection: 'postgres://localhost/bookshelf_test'
   },
 
-  production: {}
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
