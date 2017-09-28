5d339b286ac855035280239f1cc080f3cf31d924
Tyler Anderson <andersontyler65@gmail.com>
2017-09-28 14:33:56 -0600

    Bookshelf

--- seeds/users.js
+++ seeds/users.js
@@ -1,11 +1,10 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
-        // Inserts seed entries
-        knex('users').insert
-        [{
+      return knex('users').insert(
+        [
+          {
               id: 1,
               first_name: 'Joanne',
               last_name: 'Rowling',
@@ -13,7 +12,8 @@ exports.seed = function(knex, Promise) {
               hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',  // youreawizard
               created_at: new Date('2016-06-29 14:26:16 UTC'),
               updated_at: new Date('2016-06-29 14:26:16 UTC')
-        }]
+      }
+    ])
         //knex('users').insert({id: 2, colName: 'rowValue2'}),
         //knex('users').insert({id: 3, colName: 'rowValue3'})
     });
