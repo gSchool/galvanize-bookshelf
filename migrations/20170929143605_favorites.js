cefe94f0ba607249830bc67601661a464e286b72
Tyler Anderson <andersontyler65@gmail.com>
2017-09-29 15:21:04 -0600

    Bookshelf

---
+++ migrations/20170929143605_favorites.js
@@ -0,0 +1,14 @@
+'use strict'
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').notNullable().references('users.id').onDelete('CASCADE').index()
+    table.integer('book_id').notNullable().references('books.id').onDelete('CASCADE').index()
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('favorites')
+
+};
