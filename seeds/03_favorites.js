ca032980af46567908ed84785c589cf2f24196a7
Tyler Anderson <andersontyler65@gmail.com>
2017-09-29 15:37:05 -0600

    bookshelf-part5

---
+++ seeds/03_favorites.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('favorites').del()
+    .then(function () {
+      return knex('favorites').insert(
+        // Inserts seed entries
+      [
+        {
+          id: 1,
+          book_id: 1,
+          user_id: 1,
+          created_at: new Date('2016-06-29 14:26:16 UTC'),
+          updated_at: new Date('2016-06-29 14:26:16 UTC')
+        }
+      ]);
+    });
+};
