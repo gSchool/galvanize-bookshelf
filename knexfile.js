0f6c6fdc8295b93edf57fc44831db8f21bc685de
brain-kracha <brian.kracha@gmail.com>
2017-12-06 20:30:29 -0700

    changed the postgres connection in knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   development: {client:'pg',
-  connection:'postgresql://localhost/bookshelf_dev'},
+  connection:'postgres://localhost/bookshelf_dev'},
 
   test: {client:'pg',
-  connection:'postgresql://localhost/bookshelf_test'},
+  connection:'postgres://localhost/bookshelf_test'},
 
   production: {client: 'pg',
 connection: process.env.DATABASE_URL}
