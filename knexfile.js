a145a0972967eb1f973f676c4a1c3031d9b1d54d
brain-kracha <brian.kracha@gmail.com>
2017-12-06 19:13:40 -0700

    migration for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = {
-  development: {},
+  development: {client:'pg',
+  connection:'postgresql://localhost/bookshelf_dev'},
 
-  test: {},
+  test: {client:'pg',
+  connection:'postgresql://localhost/bookshelf_test'},
 
-  production: {}
+  production: {client: 'pg',
+connection: process.env.DATABASE_URL}
 };
