101b8b93f7abacc8b79478aa3ce2f5ebc7205646
Teddi Maull <teddimaull@gmail.com>
2018-11-16 16:50:10 -0700

    Turn off CSRF protection so students can troubleshoot

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ const path = require('path')
 app.use(express.static(path.join('public')))
 
 // CSRF protection
-app.use((req, res, next) => {
-  if (/json/.test(req.get('Accept'))) {
-    return next()
-  }
-
-  res.sendStatus(406)
-})
+// app.use((req, res, next) => {
+//   if (/json/.test(req.get('Accept'))) {
+//     return next()
+//   }
+//
+//   res.sendStatus(406)
+// })
 
 const books = require('./routes/books')
 const favorites = require('./routes/favorites')
