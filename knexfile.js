453b69087ae64d5d2b488424dbcf78fcf9581b69
Teddi Maull <teddim@users.noreply.github.com>
2018-11-24 21:44:58 -0700

    Revert "Pull for updated bookshelf branch"

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,9 @@
 'use strict';
 
 module.exports = {
-  development: {client:'pg',
-  connection:'postgres://localhost/bookshelf_dev'},
+  development: {},
 
-  test: {client:'pg',
-  connection:'postgres://localhost/bookshelf_test'},
+  test: {},
 
-  production: {client: 'pg',
-connection: process.env.HEROKU_POSTGRESQL_CRIMSON_URL}
+  production: {}
 };
--- routes/books.js
+++ routes/books.js
@@ -1,93 +1,10 @@
 'use strict';
 
 const express = require('express');
-const knex = require('../knex.js');
- const router = express.Router();
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
 
 // YOUR CODE HERE
-router.get('/books',(req,res,next)=>{
-  return knex('books')
-  .orderBy('title', 'ASC')
-.select('id','title','author','genre','description','cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt')
-.then((books)=>{
-  res.send(books)
-    })
-  .catch((err)=> {
-    next(err)
-})
-});
-
-router.get('/books/:id',(req,res,next)=> {
-  return knex('books')
-  .select('id','title','author','genre','description','cover_url AS coverUrl', 'created_at AS createdAt', 'updated_at AS updatedAt').first()
-  .where('id', req.params.id)
-  .then(book =>{
-    if(!book){
-      return next(err)
-    }
-    res.send(book)
-  })
-  .catch((err)=>{
-    next(err)
-  })
-})
-
-router.post('/books', (req,res,next)=>{
- return knex('books')
-  .insert({title : req.body.title,
-          author: req.body.author,
-          genre: req.body.genre,
-          description: req.body.description,
-          cover_url:req.body.coverUrl },'*')
-        .then((book)=>{
-          const newBook = {
-          id: book[0].id,
-          title: book[0].title,
-          author: book[0].author,
-          genre: book[0].genre,
-          description: book[0].description,
-          coverUrl :book[0].cover_url
-
-        }
-        res.status(200).send(newBook)
-        })
-        .catch((err)=>{
-          next(err)
-        })
-})
-
-router.patch('/books/:id', (req,res,next)=>{
-      return knex('books')
-        .where('id', req.params.id)
-      .first()
-      .update({title : req.body.title,
-              author: req.body.author,
-              genre: req.body.genre,
-             description: req.body.description,
-            cover_url:req.body.coverUrl})
-
-          .returning(['id','title','author','genre','description','cover_url AS coverUrl'])
-  // })
-  .then(updatedBook =>{
-  res.send(updatedBook[0])
-  })
-  .catch((err)=>{
-    next(err)
-  })
-})
-
-router.delete('/books/:id',(req,res,next)=>{
-  let rows = ['title','author','genre','description','cover_url AS coverUrl']
-  return knex('books')
-  .where('id',req.params.id)
-  .del()
-  .first(rows)
-  .then((deleted)=>{
-    res.status(200).send(deleted)
-  })
-
-});
-
-
 
 module.exports = router;
